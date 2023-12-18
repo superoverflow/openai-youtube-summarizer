cb90342f7821adec808281ad4e7a9d3fdfdbb3fe
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-18 21:46:15 +0000

    add search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full items-center justify-between font-mono flex-row">
-        <p>
-          Search a youtube video to Summarizer
-          <Button>Search</Button>
-        </p>
+      <div className="flex w-full flex-row items-center space-between font-mono">
+        <Input placeholder="Search for youtube videos to summarize"/>
+        <Button className="m-2">Search</Button>
       </div>
     </main>
   );
