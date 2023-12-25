950ae1cd1bf07fd3802dce4e17d498db8f6fe3dc
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-25 14:10:22 +0000

    add analysing spin

--- app/ui/run-analysis-button.tsx
+++ app/ui/run-analysis-button.tsx
@@ -3,18 +3,30 @@
 import { PiCheckFatFill } from "react-icons/pi";
 import { Button } from "@/components/ui/button";
 import { processVideo } from "@/app/lib/actions";
+import { IoMdHourglass } from "react-icons/io";
 
-import { useTransition } from 'react';
+import { useTransition, useState } from 'react';
 
 export const RunAnalysisButton = ({ videoId }: { videoId: string }) => {
-    let [isPending, startTransition] = useTransition();
+  const [_, startTransition] = useTransition();
+  const [sent, setSent] = useState(false);
 
-    return (
-        <Button className="w-full" variant="outline" 
-            onClick={() => startTransition(() => processVideo(videoId))}
-        >
-            <PiCheckFatFill className="mr-2 h-4 w-4" />
-            <span>{isPending ? "..." : "Run Analysis"}</span>
-        </Button>
-    )
+  return (
+    <Button className="w-full" variant="outline"
+      onClick={() => startTransition(() => {
+        setSent(true)
+        processVideo(videoId)
+      }
+      )}
+      disabled={sent}
+      aria-disabled={sent}
+    >
+      {
+        sent ? <IoMdHourglass className="mr-2 h-4 w-4 animate-spin" /> :
+          <PiCheckFatFill className="mr-2 h-4 w-4" />
+
+      }
+      <span>{sent ? "Analyzing" : "Run Analysis"}</span>
+    </Button>
+  )
 }
