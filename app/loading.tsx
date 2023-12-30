7945686b498eee29b60b9a5dbef266db258895aa
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-12-30 15:09:05 +0000

    center loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export default function Loading() {
   return (
-    <div className="container flex mx-auto h-screen w-full justify-center align-middle">
-      <AiOutlineLoading className="animate-spin h-12 w-12 mt-4" />
+    <div className="container flex mx-auto min-h-screen w-full justify-center items-center">
+      <AiOutlineLoading className="animate-spin h-4 w-4" />
     </div>
   )
 }
